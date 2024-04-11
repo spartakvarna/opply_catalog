158939d648d404c4719e97160e40d5b550fa21f6
Dilyan Dokov <dilyan.dokov@gmail.com>
2024-04-11 11:15:29 +0300

    Add Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Model of the Product entity."""
+
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
