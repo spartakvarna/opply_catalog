adb50b4b590a42efdcb3af64508ed0f6f02a2eba
Dilyan Dokov <dilyan.dokov@gmail.com>
2024-04-11 15:18:00 +0300

    Index Order by Customer

--- products/models.py
+++ products/models.py
@@ -31,7 +31,7 @@ class Order(models.Model):
 
     datetime = models.DateTimeField(auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)  # Products should be soft-deleted always
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)  # Products should be soft-deleted always
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, db_index=True)  # Products should be soft-deleted always
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
 
